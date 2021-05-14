41536378562f233c9fa8c67a52bac07431282592
Yuvrajhere <yuvrajisbest13@gmail.com>
2021-05-14 17:24:19 +0530

    Added User Signup, Login and checkToken feature

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const { signin, checkToken } = require("../controllers/auth");
+const { addUser } = require("../controllers/user");
+
+// creating a user
+router.post("/", addUser);
+
 module.exports = router;
